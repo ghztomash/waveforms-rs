4b5347c4bf8b13c189e6d455677207cbd4fa49e5
Tomash Ghz <ghz.tomash@hotmail.com>
2023-12-19 03:14:35 +0100

    Second waveform example

--- examples/plot.rs
+++ examples/plot.rs
@@ -1,23 +1,42 @@
 use rgb::RGB8;
 use textplots::{Chart, ColorPlot, Shape};
-use waveforms_rs::Waveform;
+use waveforms_rs::{Waveform, WaveformType};
 
 fn main() {
     let mut wave = Waveform::new(1000.0, 20.0);
 
-    let mut values = Vec::new();
+    let mut sin_values = Vec::new();
     for i in 0..100 {
-        values.push((i as f32, wave.process()));
+        sin_values.push((i as f32, wave.process()));
     }
 
-    let point_color = RGB8 {
-        r: 255,
-        g: 0,
-        b: 255,
-    };
+    wave.set_waveform_type(WaveformType::Sawtooth);
+    wave.set_amplitude(0.5);
+    wave.set_dc_offset(0.5);
+    wave.reset();
 
-    println!("sin()");
+    let mut saw_values = Vec::new();
+    for i in 0..100 {
+        saw_values.push((i as f32, wave.process()));
+    }
+
+    println!("Waveforms");
     Chart::new(160, 80, 0.0, 100.0)
-        .linecolorplot(&Shape::Lines(&values), point_color)
+        .linecolorplot(
+            &Shape::Lines(&saw_values),
+            RGB8 {
+                r: 0,
+                g: 255,
+                b: 255,
+            },
+        )
+        .linecolorplot(
+            &Shape::Lines(&sin_values),
+            RGB8 {
+                r: 255,
+                g: 0,
+                b: 255,
+            },
+        )
         .display();
 }
