5df9c1f60d2950f3d129d7077428ee3c8101a244
Tomash Ghz <ghz.tomash@hotmail.com>
2023-12-19 02:17:50 +0100

    Color plot example

--- examples/plot.rs
+++ examples/plot.rs
@@ -1,9 +1,17 @@
-use textplots::{Chart, Plot, Shape};
+use rgb::RGB8;
+use textplots::{Chart, ColorPlot, Shape};
+use waveforms_rs::waveforms::Waveform;
 
 fn main() {
-    println!("y = sin(x) / x");
+    let mut wave = Waveform::default();
 
-    Chart::default()
-        .lineplot(&Shape::Continuous(Box::new(|x| x.sin() / x)))
+    let mut values = Vec::new();
+    for i in 0..440 {
+        values.push((i as f32, wave.process()));
+    }
+
+    println!("sin()");
+    Chart::new(160, 80, 0.0, 441.0)
+        .linecolorplot(&Shape::Lines(&values), RGB8 { r: 0, g: 0, b: 255 })
         .display();
 }
