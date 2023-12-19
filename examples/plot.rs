7bfda6302ddc84e03fe41b9ff76b644fabe857e7
Tomash Ghz <ghz.tomash@hotmail.com>
2023-12-19 02:24:54 +0100

    example graph finetuning

--- examples/plot.rs
+++ examples/plot.rs
@@ -3,15 +3,21 @@ use textplots::{Chart, ColorPlot, Shape};
 use waveforms_rs::waveforms::Waveform;
 
 fn main() {
-    let mut wave = Waveform::default();
+    let mut wave = Waveform::new(1000.0, 20.0);
 
     let mut values = Vec::new();
-    for i in 0..440 {
+    for i in 0..100 {
         values.push((i as f32, wave.process()));
     }
 
+    let point_color = RGB8 {
+        r: 255,
+        g: 0,
+        b: 255,
+    };
+
     println!("sin()");
-    Chart::new(160, 80, 0.0, 441.0)
-        .linecolorplot(&Shape::Lines(&values), RGB8 { r: 0, g: 0, b: 255 })
+    Chart::new(160, 80, 0.0, 100.0)
+        .linecolorplot(&Shape::Lines(&values), point_color)
         .display();
 }
